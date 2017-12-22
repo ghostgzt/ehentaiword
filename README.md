f229ed93992244874413b958d8c8893bb0b29f0c
Gentle Kwan <ghostgzt@gmail.com>
2017-12-23 02:36:55 +0800

    update 2017.12.23

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ehentaiword
 ExHentai搜索关键词文本解析
+Copyright (C) 2017 Gentle Kwan Under GPL
---
+++ w.php
@@ -0,0 +1,28 @@
+<?php
+header("Content-Type:text/html;charset=UTF-8");
+$w=file_get_contents("w.txt");
+$w=explode("\r\n",$w);
+$out=array();
+$group="";
+foreach($w as $key=>$value){
+	if(!$value){
+		continue;
+	}
+	if(is_numeric(substr($value,0,1)) and substr($value,1,1)=="."){
+		$group=substr($value,2);
+		$out[$group]=array();
+	}else{
+		if(strstr($value,"=")){
+			$a=explode("=",$value);
+			if(preg_match("/^[A-Za-z0-9]+$/",$value)){
+				$k=$a[1];
+				$v=$a[0];
+			}else{
+				$k=$a[0];
+				$v=$a[1];
+			}
+			array_push($out[$group],array("key"=>$k,"value"=>$v));
+		}
+	}
+}
+die(json_encode($out));
